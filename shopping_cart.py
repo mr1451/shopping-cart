f126c1311c28bcdff754b10a3d2b2d6235a71288
mr1451 <mr1451@georgetown.edu>
2020-02-04 14:09:19 -0500

    Capture user input.

--- shopping_cart.py
+++ shopping_cart.py
@@ -25,7 +25,22 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
+# print(products)
 # pprint(products)
+
+#
+# INFO CAPTURE
+#
+
+selected_id = input("Please input a product identifier:") #> "9" (string)
+print(selected_id)
+print(type(selected_id))
+
+
+
+
+#
+# INFO DISPLAY
+# 
 
 # TODO: write some Python code here to produce the desired output
